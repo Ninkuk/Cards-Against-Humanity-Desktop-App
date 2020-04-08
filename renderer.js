13662ee7ab246f46cf0c387cb444c987cc94eb1f
Ninad Kulkarni <ninadk03@gmail.com>
2020-04-07 22:32:52 -0700

    implemented frameless window with custom controls

---
+++ renderer.js
@@ -0,0 +1,11 @@
+const{ remote, ipcRenderer} = require('electron')
+
+document.getElementById('close-btn').addEventListener('click', () => {
+
+    remote.getCurrentWindow().close()
+})
+
+document.getElementById('min-btn').addEventListener('click', () => {
+    console.log('bruh');
+    remote.getCurrentWindow().minimize();
+})
