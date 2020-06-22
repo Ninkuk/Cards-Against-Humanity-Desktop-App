11fc421c03a2c3ecefb797136ae5e8c2a050d096
Ninad Kulkarni <ninadk03@gmail.com>
2020-06-22 06:32:20 -0700

    Partly implemented selection of white cards
    Signed-off-by: ninkuk <ninadk03@gmail.com>

--- js/gameplay.js
+++ js/gameplay.js
@@ -1,30 +1,29 @@
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-
-function drag(ev, cardNum) {
-    ev.dataTransfer.setData("cardNum", cardNum);
-    document.getElementById('drop-area').style.display = 'block';
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("cardNum");
-    console.log(data);
-    // ev.target.appendChild(document.getElementById('white-card-0'));
-    document.getElementById('drop-area').style.display = 'none';
-}
-
-
 var fs = require('fs');
 var pack = JSON.parse(fs.readFileSync('./json/Full_Pack.json', 'utf8'));
 
 var blackCards = JSON.parse(sessionStorage.getItem('blackCards'));
 var myHand = JSON.parse(sessionStorage.getItem('myHand'));
 
-document.getElementById('black-card-text').innerText = pack["blackCards"][blackCards[0]]["text"];
+let firstBlackCard = pack["blackCards"][blackCards[0]];
+document.getElementById('black-card-text').innerText = firstBlackCard["text"];
+
+let card = document.getElementsByClassName("white-card");
+let cardsAllowed = firstBlackCard["pick"];
+let cardsSelectedCount = 0;
+for (let i = 0; i < card.length; i++) {
+
+    card[i].addEventListener("click", () => {
+        if (card[i].classList.contains("selectedOption")) {
+            card[i].classList.remove("selectedOption");
+            cardsSelectedCount--;
+        } else if (cardsSelectedCount < cardsAllowed) {
+            card[i].classList.add("selectedOption")
+            cardsSelectedCount++;
+        }
+    })
+}
 
 for (let index = 0; index < 10; index++) {
-    document.getElementById(`white-card-${index}`).innerText = myHand[index];
+    document.getElementById(`white-card-text-${index}`).innerText = myHand[index];
     console.log(myHand[index])
 }
